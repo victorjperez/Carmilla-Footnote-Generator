f96d4e537b5588e0a9ee653f7374a15c6cd65e06
Jade <jjm0004@mix.wvu.edu>
2019-11-13 13:34:05 -0500

    Updated Summary Generator

--- interpreter.py
+++ interpreter.py
@@ -18,78 +18,70 @@ from nltk.tokenize import word_tokenize, sent_tokenize
   # #book year
   # year = ''
 
-def _create_frequency_table(text_string) -> dict:
-  
-    stopWords = set(stopwords.words("english"))
-    words = word_tokenize(text_string)
+# frequency table of entire novel = text
+def getFreqTable(text) -> dict:
+
+    stop_words = set(stopwords.words("english"))
+    words = word_tokenize(text)
     ps = PorterStemmer()
 
-    freqTable = dict()
+    freq_table = dict()
     for word in words:
         word = ps.stem(word)
-        if word in stopWords:
+        if word in stop_words:
             continue
-        if word in freqTable:
-            freqTable[word] += 1
+        if word in freq_table:
+            freq_table[word] += 1
         else:
-            freqTable[word] = 1
+            freq_table[word] = 1
 
-    return freqTable
+    return freq_table
 
-def _score_sentences(sentences, freqTable) -> dict:
-    sentenceValue = dict()
+def scoreSentences(sentences, freq_table) -> dict:
+    sentence_value = dict()
 
     for sentence in sentences:
         word_count_in_sentence = (len(word_tokenize(sentence)))
-        for wordValue in freqTable:
-            if wordValue in sentence.lower():
-                if sentence[:10] in sentenceValue:
-                    sentenceValue[sentence[:10]] += freqTable[wordValue]
+        for word_value in freq_table:
+            if word_value in sentence.lower():
+                if sentence[:10] in sentence_value:
+                    sentence_value[sentence[:10]] += freq_table[word_value]
                 else:
-                    sentenceValue[sentence[:10]] = freqTable[wordValue]
+                    sentence_value[sentence[:10]] = freq_table[word_value]
 
-        sentenceValue[sentence[:10]] = sentenceValue[sentence[:10]] // word_count_in_sentence
+        sentence_value[sentence[:10]] = sentence_value[sentence[:10]] // word_count_in_sentence
 
-    return sentenceValue
+    return sentence_value
 
-def _find_average_score(sentenceValue) -> int:
-    sumValues = 0
-    for entry in sentenceValue:
-        sumValues += sentenceValue[entry]
+def avgSentenceScore(sentence_value) -> int:
+    sum_of_values = 0
+    for entry in sentence_value:
+        sum_of_values += sentence_value[entry]
 
     # Average value of a sentence from original text
-    average = int(sumValues / len(sentenceValue))
+    avg = int(sum_of_values / len(sentence_value))
 
-    return average
+    return avg
 
-def _generate_summary(sentences, sentenceValue, threshold):
+def addSummary(sentences, sentence_value, threshold):
   sentence_count = 0
   summary = ''
 
   for sentence in sentences:
-      if sentence[:10] in sentenceValue and sentenceValue[sentence[:10]] > (threshold):
+      if sentence[:10] in sentence_value and sentence_value[sentence[:10]] > (threshold):
           summary += " " + sentence
           sentence_count += 1
 
   return summary
 
   # chapter: chapter from the .txt file retrieved from gutenberg.org; in this case, Carmilla (1872)
-def summarizeText(chapter):
+def summarizeText(text, chapter):
 
-  # 1 Create the word frequency table
-  freq_table = _create_frequency_table(chapter)
-
-  # 2 Tokenize the sentences
   sentences = sent_tokenize(chapter)
-
-  # 3 Important Algorithm: score the sentences
-  sentence_scores = _score_sentences(sentences, freq_table)
-
-  # 4 Find the threshold
-  threshold = _find_average_score(sentence_scores)
-
-  # 5 Important Algorithm: Generate the summary
-  summary = _generate_summary(sentences, sentence_scores, 1.5 * threshold)
+  freq_table = getFreqTable(text)
+  sentence_scores = scoreSentences(sentences, freq_table)
+  threshold = avgSentenceScore(sentence_scores)
+  summary = addSummary(sentences, sentence_scores, 1.5 * threshold)
 
   print(summary)
 
